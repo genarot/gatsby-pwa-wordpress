8512176a6ac5611a1e121d9fd39c9b0511ba5375
Genaro Jose Tinoco Palacios <gtinoco@agssa.net>
2019-10-04 16:39:53 -0600

    Section 6: finished

--- src/layouts/PrimaryLayout.js
+++ src/layouts/PrimaryLayout.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Footer from "../components/Footer"
 import Header from "../components/Header"
+import SEO from "../components/SEO"
 
 const PrimaryLayout = (props) => {
   return (
     <div>
+      <SEO/>
       <Header activeKey={props.activeMenu || window.location.pathname}/>
       <div className="container">
         <div className="row justify-content-md-center">
